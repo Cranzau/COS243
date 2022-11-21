acad317f7e51dbce9054840e2630e43c6e0ed08e
Cranzau <104843537+Cranzau@users.noreply.github.com>
2022-11-21 10:45:10 -0500

    Add files via upload

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  root"articles#index"
+  resources :articles do
+    resources :comments
+  end
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
+
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
